5b67cc479ee0199fe5f94112e5e3464d637df01d
alohacyclist <c-mueller87@live.de>
2022-02-01 14:44:39 +0100

    iterations 1-3

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,13 @@
-// Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+// create drones in db
+const mongoose = require('mongoose');
+const Drone = require("./models/Drone.model");
+drones.forEach(drone => {
+    Drone.create(drone)
+    mongoose.connection.close()
+})
